0d1aa640c2136120161be21a1faa4c557734684f
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-08 17:33:44 -0500

    Add Marian background rays to Lourdes and bump SW cache to v27

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v26';
+const CACHE = 'tup-v27';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
