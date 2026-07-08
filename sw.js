5f4a985828a2d869e0f41833efec8a89863c0679
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-08 17:28:53 -0500

    Refine Lourdes SVG framing and bump SW cache to v24

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v23';
+const CACHE = 'tup-v24';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
