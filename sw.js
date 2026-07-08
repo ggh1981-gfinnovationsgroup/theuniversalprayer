7b2c0c0e3c758f8145455d27ba206c9556dd145f
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-08 17:23:56 -0500

    Polish jaculatoria share button and bump SW cache to v23

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v22';
+const CACHE = 'tup-v23';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
