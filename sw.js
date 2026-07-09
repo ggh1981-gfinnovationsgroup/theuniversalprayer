d4856110f05e389fd826be97641524cfe8cbff9d
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-09 16:20:21 -0500

    Reduce mobile render cost for Call Altar

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v39';
+const CACHE = 'tup-v41';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
