c7e56e8601b5d442f8dcfc1851afad23cb087bfd
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-20 10:45:28 -0500

    Add homepage kids CTA and bump cache

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v118';
+const CACHE = 'tup-v119';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
