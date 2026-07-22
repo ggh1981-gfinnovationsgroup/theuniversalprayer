348fb058273e277c1aead490483557e2dec70e99
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-22 13:47:50 -0500

    Add Padre Nuestro, Ave Maria, Gloria to each Via Crucis station (display + TTS)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v138';
+const CACHE = 'tup-v139';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
