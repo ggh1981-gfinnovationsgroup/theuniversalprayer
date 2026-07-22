027d9b31ec47c7040d4fc48c11045ae0069573bc
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-22 17:47:43 -0500

    Add artisanal stories sanjuanapostol, sanjuandedios, juanpablo (~11-18min, 54/101); cache v149

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v148';
+const CACHE = 'tup-v149';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
