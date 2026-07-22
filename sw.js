1aaafc480061347cc7e6f13372eb9c8fa7d2ef21
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-22 12:51:14 -0500

    Rewrite juanbosco, teresita, nicolas as narrative bedtime stories

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v133';
+const CACHE = 'tup-v134';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
