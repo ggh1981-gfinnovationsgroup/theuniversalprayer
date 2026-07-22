e200dbdbd0ae0d1afd173f54619eb1db8bc62d84
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-22 17:16:16 -0500

    Improve difuntos page (emotional hook, Q&A, November); add saints sanpedro/santaclara/santacatalina artisanal (48/101); cache v147

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v146';
+const CACHE = 'tup-v147';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
