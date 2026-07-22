bf4ba9a1c24a1881e7bbebae35876dd60d67554f
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-22 13:25:28 -0500

    Rewrite 8 children stories as narrative bedtime stories: fatima, divinonino, sanjose, guadalupe, carlo, mariagoretti, angelguarda, sanfelipeneri

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v134';
+const CACHE = 'tup-v135';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
