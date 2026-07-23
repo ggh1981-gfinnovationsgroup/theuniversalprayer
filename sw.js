b6f1b1f6a3b22e0a047097f1028f8d3e228d0c42
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-23 10:26:18 -0500

    Fix: swap inverted histories between providencia and divinaprovidencia; cache v150

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v149';
+const CACHE = 'tup-v150';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
