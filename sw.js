b4c29825a7303c20cbe92f0c176d52f76dda1ee6
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-23 18:03:25 -0500

    Bump cache to v162 for ultra-artisanal pass

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v161';
+const CACHE = 'tup-v162';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
