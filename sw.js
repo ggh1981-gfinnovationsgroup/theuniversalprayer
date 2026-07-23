c9967ab3f735891ff0d1e239d0b0181a4ec1a7be
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-23 12:34:57 -0500

    Rewrite histories artesanally: providencia (manna/sermon mount/Teresa/Pio) + divinaprovidencia (Roma/Santurce/Pablo VI); cache v151

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v150';
+const CACHE = 'tup-v151';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
