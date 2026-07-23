f42bfe594fe7e4d7346fc350653c5ea9b1c76822
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-23 18:14:50 -0500

    Bump cache to v165 to remove verse references

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v164';
+const CACHE = 'tup-v165';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
