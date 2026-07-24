e5003c1f822a517e64bddd6d82950bd9f52d10fc
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-24 12:48:21 -0500

    Bump service worker cache version to v176 after family JSON parse fix

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v175';
+const CACHE = 'tup-v176';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
