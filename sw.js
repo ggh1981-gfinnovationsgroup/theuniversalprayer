032bdb07f77ec1f3ec5124ad007c11c45340d531
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-24 14:44:06 -0500

    Add emergency prayer for confronting an enemy

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v178';
+const CACHE = 'tup-v179';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
