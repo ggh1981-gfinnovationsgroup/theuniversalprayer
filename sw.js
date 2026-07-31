18407f5944ca38d4cbaf4fa37de219c0f3551ba3
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-31 16:36:18 -0500

    sanguillermo: add lobo + mujer del rey Roger to miracles + cache v186

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v185';
+const CACHE = 'tup-v186';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
