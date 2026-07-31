bad22c5de6091c50dcb0e571e2dfbf5cf68ef2f0
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-07-31 16:28:30 -0500

    ninos: rewrite Gellone (warrior-to-monk) + Aquitania (count-to-hermit) Disney-quality + cache v185

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v184';
+const CACHE = 'tup-v185';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
