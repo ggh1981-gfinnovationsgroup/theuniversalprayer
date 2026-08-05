1565286688b4e073fbc52eee7f415ade9b79b7bb
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-08-05 11:33:47 -0500

    salmos-diarios: add San Miguel prayer + internal link from oscuridad section + cache v195

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v194';
+const CACHE = 'tup-v195';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
