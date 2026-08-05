f035be345a966d240e1700822473b6503562afff
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-08-05 10:42:43 -0500

    salmos-diarios: open Psalm 23 and 91 by default + cache v193

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v191';
+const CACHE = 'tup-v193';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
