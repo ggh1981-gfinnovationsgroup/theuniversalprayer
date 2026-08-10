b59dbbcf8ad4e4fb3a549136e26214479a254b39
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-08-10 17:47:03 -0500

    emergencias: remove super emergency wording from enemy prayer + cache v212

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v211';
+const CACHE = 'tup-v212';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
