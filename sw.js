71a3af88fd030e25d53383f8ace5058384cfef2c
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-08-10 12:10:43 -0500

    Bump service worker cache version

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v204';
+const CACHE = 'tup-v205';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
