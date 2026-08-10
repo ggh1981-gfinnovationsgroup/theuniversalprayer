badc522df26ebdd7e3747e9566a45a4cfcd3907b
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-08-10 15:22:08 -0500

    meses: append prayer text after prayer references (Magnificat, Padre Nuestro, etc.) + cache v208

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v207';
+const CACHE = 'tup-v208';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
