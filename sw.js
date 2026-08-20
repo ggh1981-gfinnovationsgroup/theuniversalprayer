b4ae126c11dfd00ad22fa761a1325d79774403b8
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-08-20 11:13:07 -0500

    Refocus rebirth page and bump cache

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v218';
+const CACHE = 'tup-v219';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
