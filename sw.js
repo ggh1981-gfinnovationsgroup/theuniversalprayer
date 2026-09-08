5b7e4100c6f64829bc203b2304a12b79ead76d8a
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-09-08 16:41:36 -0500

    nomascastigo: distincion 1 agrega pregunta y respuesta sobre pedir que Dios quite consecuencias (Sal 25, Jon 3); bump tup-v239

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v238';
+const CACHE = 'tup-v239';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
