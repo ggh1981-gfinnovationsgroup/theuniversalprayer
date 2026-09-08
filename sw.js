c343531c445047ed956061e8e166db8cd511a997
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-09-08 16:38:26 -0500

    nomascastigo: oración individual al final de cada distinción; bump cache tup-v238

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v237';
+const CACHE = 'tup-v238';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
