966752e196777639e1b275a7d09acacf06933a21
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-09-09 12:08:17 -0500

    vuelvo-al-padre: agrega tarjetas promesas rotas (Mt 5,37 / Ecl 5) y dano a otros (Lc 19); bump tup-v243

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v242';
+const CACHE = 'tup-v243';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
