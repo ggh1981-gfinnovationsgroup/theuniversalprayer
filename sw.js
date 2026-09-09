8eae61d7493ceb8df083b09a4e1423d2188e73f3
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-09-09 12:19:58 -0500

    vuelvo-al-padre: agrega paso para cuando no hay direccion espiritual disponible; bump tup-v245

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v244';
+const CACHE = 'tup-v245';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
