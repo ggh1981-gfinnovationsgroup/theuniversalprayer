4c7241c344fa1117af3d3455a2129b3848877d7c
Guillermo Gomez <guillermo.gomez@bwh.com>
2026-09-09 12:46:22 -0500

    home: mueve camino pastoral justo despues de 15-minutos adoracion; bump tup-v251

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy: works fully offline after first load
    ===================================================== */
 
-const CACHE = 'tup-v250';
+const CACHE = 'tup-v251';
 
 // All files to pre-cache on install
 const PRECACHE_URLS = [
